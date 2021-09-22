0aa1e2a860ac6f506e4ef393e27ab5c2b688bb60
renoalane <renopradino@gmail.com>
2021-09-22 12:37:49 +0700

    permission geolocation

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+function succes(position) {
+  console.log(position);
+}
+
+function error() {
+  alert("Posisi tidak dapat diakses");
+}
+
+function userLocation() {
+  if (!navigator.geolocation) {
+    alert("Geo Location Tidak didukung didalam Browser Anda, Silahkan gunakan browser lain");
+  } else {
+    navigator.geolocation.getCurrentPosition(succes, error);
+  }
+}
+
 function index() {
   let app = document.querySelector("#app");
   let h3 = document.createElement("h3");
   h3.innerHTML = "Prayer Times";
 
   app.appendChild(h3);
+  userLocation();
 }
 
 index();
