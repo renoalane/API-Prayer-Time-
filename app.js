1c85edcf73e96c7f0ce3d795f340295244c1ddda
renoalane <renopradino@gmail.com>
2021-09-22 13:46:07 +0700

    Membuat Table untuk Jadwal Sholat

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ function prayerTime(latitude, longitude) {
     .then(function (response) {
       let date = new Date();
       let today = date.getDate() - 1;
-      console.log(today);
-      console.log(response.data[today]);
+      let data = response.data[today].timings;
+
+      let app = document.querySelector("#app");
+      let table = document.createElement("table");
+      let tableTbody = document.createElement("tbody");
+
+      for (i in data) {
+        let row = tableTbody.insertRow();
+        let name = row.insertCell(0);
+        let time = row.insertCell(1);
+        name.innerHTML = i;
+        time.innerHTML = data[i];
+
+        tableTbody.appendChild(row);
+      }
+      table.appendChild(tableTbody);
+      app.appendChild(table);
     });
 }
 
